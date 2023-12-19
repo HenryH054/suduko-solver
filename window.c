db8098d43844e49c5b8c221b87f024575ab97042
HenryH054 <henryphutchinson@gmail.com>
2023-12-18 19:17:03 -0700

    removed deprecated function

--- window.c
+++ window.c
@@ -13,7 +13,8 @@ int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("Suduko_Solver", G_APPLICATION_DEFAULT_FLAGS);
+  app =
+      gtk_application_new("org.example.HelloGTK", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
