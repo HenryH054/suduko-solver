2965750a934b5524264eddbb76a9a81d179ca7d9
HenryH054 <henryphutchinson@gmail.com>
2023-12-18 19:22:38 -0700

    cleaned up some boilerplate

--- window.c
+++ window.c
@@ -4,8 +4,8 @@ static void on_activate(GApplication *app, gpointer user_data) {
   GtkWidget *window;
 
   window = gtk_application_window_new(GTK_APPLICATION(app));
-  gtk_window_set_title(GTK_WINDOW(window), "Hello GTK!");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  gtk_window_set_title(GTK_WINDOW(window), "Suduko Solver");
+  gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
   gtk_widget_show_all(window);
 }
 
@@ -13,8 +13,7 @@ int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app =
-      gtk_application_new("org.example.HelloGTK", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new("my.org.SudukoSolver", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
